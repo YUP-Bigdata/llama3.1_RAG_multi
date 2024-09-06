cf94ec99f2f786a4922a7600e8cc7d7e7939d278
이준엽 <joony0830@gmail.com>
2024-09-06 07:57:28 +0000

    Initial commit

--- config/config.py
+++ config/config.py
@@ -4,9 +4,9 @@ import os
 
 # Elasticsearch Configuration
 ES_USER = "elastic"
-ES_PASSWORD = "*MogUVM0Fu5pIm6Ku3QM"
-ES_URL = "http://192.168.10.10:9200/"
-ES_INDEX = "langchain_index_chunk_cut7_e5_exaone"
-ES_HISTORY_INDEX = "langchin_history"
+ES_PASSWORD = "************"
+ES_URL = ""************""
+ES_INDEX = ""************""
+ES_HISTORY_INDEX = ""************""
 # HuggingFace Token
-os.environ['HF_TOKEN'] = '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'
+os.environ['HF_TOKEN'] = '"************"'
